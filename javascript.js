687cee321362fbe3c929810e1ef5c909e5e62c52
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2015-08-05 09:24:39 +0800

    add javasctipt.js

---
+++ javascript.js
@@ -0,0 +1,31 @@
+/*
+var i = 0
+    while(i < 10)
+    {
+    	console.log(1);
+    	i = i+1;
+    }
+*/
+/*
+for(var i=0; i<10; i++)
+{
+	console.log(2);
+}
+*/
+
+//陣列  push加到陣列尾端
+
+/*
+var dmobject={}
+dmobject.name = "Andrew";
+dmobject.number	= 50;
+dmobject.dmfunction = function()
+
+{
+	console.log(this.name);
+}
+
+dmobject.dmfunction();
+console.log( dmobject.number );
+console.log( dmobject.name );
+*/
